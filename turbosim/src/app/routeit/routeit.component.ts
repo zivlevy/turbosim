b1c4aee90f3f104dc38dcbb1be9c76422330cb7a
ZIV LEVY <zivilevy@gmail.com>
2017-03-11 19:27:02 +0200

    Added ability to edit all route params

---
+++ turbosim/src/app/routeit/routeit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'routeit',
+  templateUrl: './routeit.component.html',
+  styleUrls: ['./routeit.component.css']
+})
+export class RouteitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
